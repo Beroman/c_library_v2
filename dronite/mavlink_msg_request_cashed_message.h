bca3e2adf8256dd54af5d09d9adaac49136fa41e
Roman Bekas <bekas.roman@gmail.com>
2022-02-10 19:31:56 +0200

    Added hop number and hop delay

--- dronite/mavlink_msg_request_cashed_message.h
+++ dronite/mavlink_msg_request_cashed_message.h
@@ -5,16 +5,17 @@
 
 
 typedef struct __mavlink_request_cashed_message_t {
-
+ uint16_t frequency_hop; /*<  Number of frequency hop*/
+ uint16_t hop_delay; /*< [ms] Delay for hop*/
 } mavlink_request_cashed_message_t;
 
-#define MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_LEN 0
-#define MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_MIN_LEN 0
-#define MAVLINK_MSG_ID_13002_LEN 0
-#define MAVLINK_MSG_ID_13002_MIN_LEN 0
+#define MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_LEN 4
+#define MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_MIN_LEN 4
+#define MAVLINK_MSG_ID_13002_LEN 4
+#define MAVLINK_MSG_ID_13002_MIN_LEN 4
 
-#define MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_CRC 0
-#define MAVLINK_MSG_ID_13002_CRC 0
+#define MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_CRC 34
+#define MAVLINK_MSG_ID_13002_CRC 34
 
 
 
@@ -22,14 +23,18 @@ typedef struct __mavlink_request_cashed_message_t {
 #define MAVLINK_MESSAGE_INFO_REQUEST_CASHED_MESSAGE { \
     13002, \
     "REQUEST_CASHED_MESSAGE", \
-    0, \
-    {  } \
+    2, \
+    {  { "frequency_hop", NULL, MAVLINK_TYPE_UINT16_T, 0, 0, offsetof(mavlink_request_cashed_message_t, frequency_hop) }, \
+         { "hop_delay", NULL, MAVLINK_TYPE_UINT16_T, 0, 2, offsetof(mavlink_request_cashed_message_t, hop_delay) }, \
+         } \
 }
 #else
 #define MAVLINK_MESSAGE_INFO_REQUEST_CASHED_MESSAGE { \
     "REQUEST_CASHED_MESSAGE", \
-    0, \
-    {  } \
+    2, \
+    {  { "frequency_hop", NULL, MAVLINK_TYPE_UINT16_T, 0, 0, offsetof(mavlink_request_cashed_message_t, frequency_hop) }, \
+         { "hop_delay", NULL, MAVLINK_TYPE_UINT16_T, 0, 2, offsetof(mavlink_request_cashed_message_t, hop_delay) }, \
+         } \
 }
 #endif
 
@@ -39,19 +44,23 @@ typedef struct __mavlink_request_cashed_message_t {
  * @param component_id ID of this component (e.g. 200 for IMU)
  * @param msg The MAVLink message to compress the data into
  *
-
+ * @param frequency_hop  Number of frequency hop
+ * @param hop_delay [ms] Delay for hop
  * @return length of the message in bytes (excluding serial stream start sign)
  */
-static inline uint16_t mavlink_msg_request_cashed_message_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg)
+static inline uint16_t mavlink_msg_request_cashed_message_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
+                               uint16_t frequency_hop, uint16_t hop_delay)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_LEN];
-
+    _mav_put_uint16_t(buf, 0, frequency_hop);
+    _mav_put_uint16_t(buf, 2, hop_delay);
 
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_LEN);
 #else
     mavlink_request_cashed_message_t packet;
-
+    packet.frequency_hop = frequency_hop;
+    packet.hop_delay = hop_delay;
 
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_LEN);
 #endif
@@ -66,20 +75,24 @@ static inline uint16_t mavlink_msg_request_cashed_message_pack(uint8_t system_id
  * @param component_id ID of this component (e.g. 200 for IMU)
  * @param chan The MAVLink channel this message will be sent over
  * @param msg The MAVLink message to compress the data into
-
+ * @param frequency_hop  Number of frequency hop
+ * @param hop_delay [ms] Delay for hop
  * @return length of the message in bytes (excluding serial stream start sign)
  */
 static inline uint16_t mavlink_msg_request_cashed_message_pack_chan(uint8_t system_id, uint8_t component_id, uint8_t chan,
-                               mavlink_message_t* msg)
+                               mavlink_message_t* msg,
+                                   uint16_t frequency_hop,uint16_t hop_delay)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_LEN];
-
+    _mav_put_uint16_t(buf, 0, frequency_hop);
+    _mav_put_uint16_t(buf, 2, hop_delay);
 
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_LEN);
 #else
     mavlink_request_cashed_message_t packet;
-
+    packet.frequency_hop = frequency_hop;
+    packet.hop_delay = hop_delay;
 
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_LEN);
 #endif
@@ -98,7 +111,7 @@ static inline uint16_t mavlink_msg_request_cashed_message_pack_chan(uint8_t syst
  */
 static inline uint16_t mavlink_msg_request_cashed_message_encode(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg, const mavlink_request_cashed_message_t* request_cashed_message)
 {
-    return mavlink_msg_request_cashed_message_pack(system_id, component_id, msg);
+    return mavlink_msg_request_cashed_message_pack(system_id, component_id, msg, request_cashed_message->frequency_hop, request_cashed_message->hop_delay);
 }
 
 /**
@@ -112,27 +125,30 @@ static inline uint16_t mavlink_msg_request_cashed_message_encode(uint8_t system_
  */
 static inline uint16_t mavlink_msg_request_cashed_message_encode_chan(uint8_t system_id, uint8_t component_id, uint8_t chan, mavlink_message_t* msg, const mavlink_request_cashed_message_t* request_cashed_message)
 {
-    return mavlink_msg_request_cashed_message_pack_chan(system_id, component_id, chan, msg);
+    return mavlink_msg_request_cashed_message_pack_chan(system_id, component_id, chan, msg, request_cashed_message->frequency_hop, request_cashed_message->hop_delay);
 }
 
 /**
  * @brief Send a request_cashed_message message
  * @param chan MAVLink channel to send the message
  *
-
+ * @param frequency_hop  Number of frequency hop
+ * @param hop_delay [ms] Delay for hop
  */
 #ifdef MAVLINK_USE_CONVENIENCE_FUNCTIONS
 
-static inline void mavlink_msg_request_cashed_message_send(mavlink_channel_t chan)
+static inline void mavlink_msg_request_cashed_message_send(mavlink_channel_t chan, uint16_t frequency_hop, uint16_t hop_delay)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_LEN];
-
+    _mav_put_uint16_t(buf, 0, frequency_hop);
+    _mav_put_uint16_t(buf, 2, hop_delay);
 
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE, buf, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_MIN_LEN, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_LEN, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_CRC);
 #else
     mavlink_request_cashed_message_t packet;
-
+    packet.frequency_hop = frequency_hop;
+    packet.hop_delay = hop_delay;
 
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE, (const char *)&packet, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_MIN_LEN, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_LEN, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_CRC);
 #endif
@@ -146,7 +162,7 @@ static inline void mavlink_msg_request_cashed_message_send(mavlink_channel_t cha
 static inline void mavlink_msg_request_cashed_message_send_struct(mavlink_channel_t chan, const mavlink_request_cashed_message_t* request_cashed_message)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
-    mavlink_msg_request_cashed_message_send(chan,);
+    mavlink_msg_request_cashed_message_send(chan, request_cashed_message->frequency_hop, request_cashed_message->hop_delay);
 #else
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE, (const char *)request_cashed_message, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_MIN_LEN, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_LEN, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_CRC);
 #endif
@@ -160,16 +176,18 @@ static inline void mavlink_msg_request_cashed_message_send_struct(mavlink_channe
   is usually the receive buffer for the channel, and allows a reply to an
   incoming message with minimum stack space usage.
  */
-static inline void mavlink_msg_request_cashed_message_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan)
+static inline void mavlink_msg_request_cashed_message_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan,  uint16_t frequency_hop, uint16_t hop_delay)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char *buf = (char *)msgbuf;
-
+    _mav_put_uint16_t(buf, 0, frequency_hop);
+    _mav_put_uint16_t(buf, 2, hop_delay);
 
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE, buf, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_MIN_LEN, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_LEN, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_CRC);
 #else
     mavlink_request_cashed_message_t *packet = (mavlink_request_cashed_message_t *)msgbuf;
-
+    packet->frequency_hop = frequency_hop;
+    packet->hop_delay = hop_delay;
 
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE, (const char *)packet, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_MIN_LEN, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_LEN, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_CRC);
 #endif
@@ -181,6 +199,25 @@ static inline void mavlink_msg_request_cashed_message_send_buf(mavlink_message_t
 // MESSAGE REQUEST_CASHED_MESSAGE UNPACKING
 
 
+/**
+ * @brief Get field frequency_hop from request_cashed_message message
+ *
+ * @return  Number of frequency hop
+ */
+static inline uint16_t mavlink_msg_request_cashed_message_get_frequency_hop(const mavlink_message_t* msg)
+{
+    return _MAV_RETURN_uint16_t(msg,  0);
+}
+
+/**
+ * @brief Get field hop_delay from request_cashed_message message
+ *
+ * @return [ms] Delay for hop
+ */
+static inline uint16_t mavlink_msg_request_cashed_message_get_hop_delay(const mavlink_message_t* msg)
+{
+    return _MAV_RETURN_uint16_t(msg,  2);
+}
 
 /**
  * @brief Decode a request_cashed_message message into a struct
@@ -191,7 +228,8 @@ static inline void mavlink_msg_request_cashed_message_send_buf(mavlink_message_t
 static inline void mavlink_msg_request_cashed_message_decode(const mavlink_message_t* msg, mavlink_request_cashed_message_t* request_cashed_message)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
-
+    request_cashed_message->frequency_hop = mavlink_msg_request_cashed_message_get_frequency_hop(msg);
+    request_cashed_message->hop_delay = mavlink_msg_request_cashed_message_get_hop_delay(msg);
 #else
         uint8_t len = msg->len < MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_LEN? msg->len : MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_LEN;
         memset(request_cashed_message, 0, MAVLINK_MSG_ID_REQUEST_CASHED_MESSAGE_LEN);
